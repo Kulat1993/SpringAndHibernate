32b5494e5b0f74c9cb20b688be54a9d292f9e8da
kamilulatowski <38046683+Kulat1993@users.noreply.github.com>
2018-10-31 18:13:50 +0100

    hb-03-one-to-many

---
+++ hb-03-one-to-many/src/com/luv2code/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,51 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+			
+			int theId = 4;
+			InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+			
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			
+			System.out.println("Assocciateded instructor: " + tempInstructorDetail.getInstructor());
+
+			if (tempInstructorDetail != null) {
+				System.out.println("Deleting...");
+				
+				// remove the assocciated link reference
+				// break bi-directional link
+				tempInstructorDetail.getInstructor().setInstructorDetail(null);
+				
+				session.delete(tempInstructorDetail);
+			}
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			// handle connection leak issue
+			session.close();
+			factory.close();
+		}
+	}
+}
